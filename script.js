caedce00b0dd62b7d1613d026ea5b36114789d59
tanushsaxena1 <166959165+tanushsaxena1@users.noreply.github.com>
2024-06-14 23:32:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById('clickMe').addEventListener('click', function() {
     alert('Button was clicked!');
 });
+
+function redirectToYoutube() {
+    window.location.href = "https://www.youtube.com/?bp=wgUCEAE%3D";
+}
